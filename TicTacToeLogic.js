a2af68e4d9b512eae42aa00d24376b22a0977bf0
Yoav Zibin <yoav.zibin@gmail.com>
2014-09-09 20:33:50 -0400

    Create TicTacToeLogic.js

---
+++ TicTacToeLogic.js
@@ -0,0 +1,148 @@
+/*jslint devel: true, indent: 2 */
+/*global console */
+var isMoveOk = (function () {
+  'use strict';
+
+  function isEqual(object1, object2) {
+    return JSON.stringify(object1) === JSON.stringify(object2);
+  }
+
+  function copyObject(object) {
+    return JSON.parse(JSON.stringify(object));
+  }
+
+  /** Return the winner (either 'X' or 'O') or '' if there is no winner. */
+  function getWinner(board) {
+    var boardString = '';
+    var i, j;
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        var cell = board[i][j];
+        boardString += (cell === '' ? ' ' : cell);
+      }
+    }
+    var win_patterns = [
+      'XXX......',
+      '...XXX...',
+      '......XXX',
+      'X..X..X..',
+      '.X..X..X.',
+      '..X..X..X',
+      'X...X...X',
+      '..X.X.X..'
+    ];
+    for (i = 0; i < win_patterns.length; i++) {
+      var win_pattern = win_patterns[i];
+      var x_regexp = new RegExp(win_pattern);
+      var o_regexp = new RegExp(win_pattern.replace(/X/g, 'O'));
+      if (x_regexp.test(boardString)) {
+        return 'X';
+      }
+      if (o_regexp.test(boardString)) {
+        return 'O';
+      }
+    }
+    return '';
+  }
+
+  /** Returns true if the game ended in a tie because there are no empty cells. */
+  function isTie(board) {
+    var i, j;
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          // If there is an empty cell then we do not have a tie.
+          return false;
+        }
+      }
+    }
+    // No empty cells --> tie!
+    return true;
+  }
+
+  /** 
+   * Returns the move that should be performed when player 
+   * with index turnIndexBeforeMove makes a move in cell row X col. 
+   */
+  function createMove(board, row, col, turnIndexBeforeMove) {
+    var boardAfterMove = copyObject(board);
+    boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'X' : 'O';
+    var winner = getWinner(boardAfterMove);
+    var firstOperation;
+    if (winner !== '' || isTie(board)) {
+      // Game over.
+      firstOperation = {endMatch: {endMatchScores: 
+        (winner === 'X' ? [1, 0] : (winner === 'O' ? [0, 1] : [0, 0]))}};
+    } else {
+      // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
+      firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
+    }
+    return [firstOperation,
+            {set: {key: 'board', value: boardAfterMove}},
+            {set: {key: 'delta', value: {row: row, col: col}}}];
+  }
+
+  function isMoveOk(params) {
+    var move = params.move; 
+    var turnIndexBeforeMove = params.turnIndexBeforeMove; 
+    var stateBeforeMove = params.stateBeforeMove; 
+    // The state and turn after move are not needed in TicTacToe (or in any game where all state is public).
+    //var turnIndexAfterMove = params.turnIndexAfterMove; 
+    //var stateAfterMove = params.stateAfterMove; 
+    
+    // We can assume that turnIndexBeforeMove and stateBeforeMove are legal, and we need
+    // to verify that move is legal.
+    try {
+      // Example move:
+      // [{setTurn: {turnIndex : 1},
+      //  {set: {key: 'board', value: [['X', '', ''], ['', '', ''], ['', '', '']]}},
+      //  {set: {key: 'delta', value: {row: 0, col: 0}}}]
+      var deltaValue = move[2].set.value;
+      var row = deltaValue.row;
+      var col = deltaValue.col;
+      var board = stateBeforeMove.board;
+      if (board === undefined) {
+        // Initially (at the beginning of the match), stateBeforeMove is {}. 
+        board = [['', '', ''], ['', '', ''], ['', '', '']];
+      }
+      // One can only make a move in an empty position 
+      if (board[row][col] !== '') {
+        return false;
+      }
+      var expectedMove = createMove(board, row, col, turnIndexBeforeMove);
+      if (!isEqual(move, expectedMove)) {
+        return false;
+      }
+    } catch (e) {
+      // if there are any exceptions then the move is illegal
+      return false;
+    }
+    return true;
+  }
+
+  // "Manual testing" --- expected result is [true, true, true, false].
+  console.log(
+    [ // Check placing X in 0x0 from initial state.
+      isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {}, 
+        move: [{setTurn: {turnIndex : 1}},
+          {set: {key: 'board', value: [['X', '', ''], ['', '', ''], ['', '', '']]}},
+          {set: {key: 'delta', value: {row: 0, col: 0}}}]}),
+      // Check placing O in 0x1 from previous state.   
+      isMoveOk({turnIndexBeforeMove: 1, 
+        stateBeforeMove: {board: [['X', '', ''], ['', '', ''], ['', '', '']], delta: {row: 0, col: 0}}, 
+        move: [{setTurn: {turnIndex : 0}},
+          {set: {key: 'board', value: [['X', 'O', ''], ['', '', ''], ['', '', '']]}},
+          {set: {key: 'delta', value: {row: 0, col: 1}}}]}),
+      // Check end game where X wins.   
+      isMoveOk({turnIndexBeforeMove: 0, 
+        stateBeforeMove: {board: [['X', 'O', ''], ['X', 'O', ''], ['', '', '']], delta: {row: 1, col: 1}}, 
+        move: [{endMatch: {endMatchScores: [1, 0]}},
+          {set: {key: 'board', value: [['X', 'O', ''], ['X', 'O', ''], ['X', '', '']]}},
+          {set: {key: 'delta', value: {row: 2, col: 0}}}]}),
+      // Checking an illegal move.
+      isMoveOk({turnIndexBeforeMove: 0, stateBeforeMove: {}, move: [{setTurn: {turnIndex : 0}}]})
+    ]);
+  
+  return isMoveOk;
+})();
+
