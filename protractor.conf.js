f18563ac77d24a70413cbb6325a06608656977eb
yoav-zibin <yoav.zibin@gmail.com>
2015-02-27 15:27:14 -0500

    EMPTY

---
+++ protractor.conf.js
@@ -0,0 +1,13 @@
+exports.config = {
+  specs: ['end_to_end_tests.js'],
+  allScriptsTimeout: 11000,
+  directConnect: true, // only works with Chrome and Firefox
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  baseUrl: 'http://localhost:9000/',
+  framework: 'jasmine',
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
