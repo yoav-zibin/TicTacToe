3efb8de731c74e24bd4ed763fdae5fdd69d806fe
yoav-zibin <yoav.zibin@gmail.com>
2015-03-04 17:32:13 -0500

    EMPTY

---
+++ dist/everything.js
@@ -0,0 +1,284 @@
+angular.module('myApp', []).factory('gameLogic', function() {
+
+  'use strict';
+
+  /** Returns the initial TicTacToe board, which is a 3x3 matrix containing ''. */
+  function getInitialBoard() {
+    return [['', '', ''],
+            ['', '', ''],
+            ['', '', '']];
+  }
+
+  /**
+   * Returns true if the game ended in a tie because there are no empty cells.
+   * E.g., isTie returns true for the following board:
+   *     [['X', 'O', 'X'],
+   *      ['X', 'O', 'O'],
+   *      ['O', 'X', 'X']]
+   */
+  function isTie(board) {
+    var i, j;
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        if (board[i][j] === '') {
+          // If there is an empty cell then we do not have a tie.
+          return false;
+        }
+      }
+    }
+    // No empty cells, so we have a tie!
+    return true;
+  }
+
+  /**
+   * Return the winner (either 'X' or 'O') or '' if there is no winner.
+   * The board is a matrix of size 3x3 containing either 'X', 'O', or ''.
+   * E.g., getWinner returns 'X' for the following board:
+   *     [['X', 'O', ''],
+   *      ['X', 'O', ''],
+   *      ['X', '', '']]
+   */
+  function getWinner(board) {
+    var boardString = '';
+    var i, j;
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        var cell = board[i][j];
+        boardString += cell === '' ? ' ' : cell;
+      }
+    }
+    var win_patterns = [
+      'XXX......',
+      '...XXX...',
+      '......XXX',
+      'X..X..X..',
+      '.X..X..X.',
+      '..X..X..X',
+      'X...X...X',
+      '..X.X.X..'
+    ];
+    for (i = 0; i < win_patterns.length; i++) {
+      var win_pattern = win_patterns[i];
+      var x_regexp = new RegExp(win_pattern);
+      var o_regexp = new RegExp(win_pattern.replace(/X/g, 'O'));
+      if (x_regexp.test(boardString)) {
+        return 'X';
+      }
+      if (o_regexp.test(boardString)) {
+        return 'O';
+      }
+    }
+    return '';
+  }
+
+  /**
+   * Returns all the possible moves for the given board and turnIndexBeforeMove.
+   * Returns an empty array if the game is over.
+   */
+  function getPossibleMoves(board, turnIndexBeforeMove) {
+    var possibleMoves = [];
+    var i, j;
+    for (i = 0; i < 3; i++) {
+      for (j = 0; j < 3; j++) {
+        try {
+          possibleMoves.push(createMove(board, i, j, turnIndexBeforeMove));
+        } catch (e) {
+          // The cell in that position was full.
+        }
+      }
+    }
+    return possibleMoves;
+  }
+
+  /**
+   * Returns the move that should be performed when player
+   * with index turnIndexBeforeMove makes a move in cell row X col.
+   */
+  function createMove(board, row, col, turnIndexBeforeMove) {
+    if (board === undefined) {
+      // Initially (at the beginning of the match), the board in state is undefined.
+      board = getInitialBoard();
+    }
+    if (board[row][col] !== '') {
+      throw new Error("One can only make a move in an empty position!");
+    }
+    if (getWinner(board) !== '' || isTie(board)) {
+      throw new Error("Can only make a move if the game is not over!");
+    }
+    var boardAfterMove = angular.copy(board);
+    boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'X' : 'O';
+    var winner = getWinner(boardAfterMove);
+    var firstOperation;
+    if (winner !== '' || isTie(boardAfterMove)) {
+      // Game over.
+      firstOperation = {endMatch: {endMatchScores:
+        winner === 'X' ? [1, 0] : winner === 'O' ? [0, 1] : [0, 0]}};
+    } else {
+      // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
+      firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
+    }
+    return [firstOperation,
+            {set: {key: 'board', value: boardAfterMove}},
+            {set: {key: 'delta', value: {row: row, col: col}}}];
+  }
+
+  function isMoveOk(params) {
+    var move = params.move;
+    var turnIndexBeforeMove = params.turnIndexBeforeMove;
+    var stateBeforeMove = params.stateBeforeMove;
+    // The state and turn after move are not needed in TicTacToe (or in any game where all state is public).
+    //var turnIndexAfterMove = params.turnIndexAfterMove;
+    //var stateAfterMove = params.stateAfterMove;
+
+    // We can assume that turnIndexBeforeMove and stateBeforeMove are legal, and we need
+    // to verify that move is legal.
+    try {
+      // Example move:
+      // [{setTurn: {turnIndex : 1},
+      //  {set: {key: 'board', value: [['X', '', ''], ['', '', ''], ['', '', '']]}},
+      //  {set: {key: 'delta', value: {row: 0, col: 0}}}]
+      var deltaValue = move[2].set.value;
+      var row = deltaValue.row;
+      var col = deltaValue.col;
+      var board = stateBeforeMove.board;
+      var expectedMove = createMove(board, row, col, turnIndexBeforeMove);
+      if (!angular.equals(move, expectedMove)) {
+        return false;
+      }
+    } catch (e) {
+      // if there are any exceptions then the move is illegal
+      return false;
+    }
+    return true;
+  }
+
+  return {
+      getInitialBoard: getInitialBoard,
+      getPossibleMoves: getPossibleMoves,
+      createMove: createMove,
+      isMoveOk: isMoveOk
+  };
+});
+;
+angular.module('myApp')
+  .controller('Ctrl',
+      ['$scope', '$log', '$timeout',
+       'gameService', 'gameLogic', 'aiService', 'resizeGameAreaService',
+      function ($scope, $log, $timeout,
+        gameService, gameLogic, aiService, resizeGameAreaService) {
+
+    'use strict';
+    
+    resizeGameAreaService.setWidthToHeight(1);
+
+    function sendComputerMove() {
+      gameService.makeMove(aiService.createComputerMove($scope.board, $scope.turnIndex,
+          // at most 1 second for the AI to choose a move (but might be much quicker)
+          {millisecondsLimit: 1000}));
+    }
+
+    function updateUI(params) {
+      $scope.board = params.stateAfterMove.board;
+      $scope.delta = params.stateAfterMove.delta;
+      if ($scope.board === undefined) {
+        $scope.board = gameLogic.getInitialBoard();
+      }
+      $scope.isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
+        params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
+      $scope.turnIndex = params.turnIndexAfterMove;
+
+      // Is it the computer's turn?
+      if ($scope.isYourTurn &&
+          params.playersInfo[params.yourPlayerIndex].playerId === '') {
+        $scope.isYourTurn = false; // to make sure the UI won't send another move.
+        // Waiting 0.5 seconds to let the move animation finish; if we call aiService
+        // then the animation is paused until the javascript finishes.
+        $timeout(sendComputerMove, 500);
+      }
+    }
+
+    // Before getting any updateUI, we show an empty board to a viewer (so you can't perform moves).
+    updateUI({stateAfterMove: {}, turnIndexAfterMove: 0, yourPlayerIndex: -2});
+
+    $scope.cellClicked = function (row, col) {
+      $log.info(["Clicked on cell:", row, col]);
+      if (!$scope.isYourTurn) {
+        return;
+      }
+      try {
+        var move = gameLogic.createMove($scope.board, row, col, $scope.turnIndex);
+        $scope.isYourTurn = false; // to prevent making another move
+        gameService.makeMove(move);
+      } catch (e) {
+        $log.info(["Cell is already full in position:", row, col]);
+        return;
+      }
+    };
+    $scope.shouldShowImage = function (row, col) {
+      var cell = $scope.board[row][col];
+      return cell !== "";
+    };
+    $scope.getImageSrc = function (row, col) {
+      var cell = $scope.board[row][col];
+      return cell === "X" ? "pieceX.png"
+          : cell === "O" ? "pieceO.png" : "";
+    };
+    $scope.shouldSlowlyAppear = function (row, col) {
+      return $scope.delta !== undefined &&
+          $scope.delta.row === row && $scope.delta.col === col;
+    };
+
+    gameService.setGame({
+      gameDeveloperEmail: "yoav.zibin@gmail.com",
+      minNumberOfPlayers: 2,
+      maxNumberOfPlayers: 2,
+      isMoveOk: gameLogic.isMoveOk,
+      updateUI: updateUI
+    });
+  }]);
+;angular.module('myApp').factory('aiService',
+    ["alphaBetaService", "gameLogic",
+      function(alphaBetaService, gameLogic) {
+
+  'use strict';
+
+  /**
+   * Returns the move that the computer player should do for the given board.
+   * alphaBetaLimits is an object that sets a limit on the alpha-beta search,
+   * and it has either a millisecondsLimit or maxDepth field:
+   * millisecondsLimit is a time limit, and maxDepth is a depth limit.
+   */
+  function createComputerMove(board, playerIndex, alphaBetaLimits) {
+    // We use alpha-beta search, where the search states are TicTacToe moves.
+    // Recal that a TicTacToe move has 3 operations:
+    // 1) endMatch or setTurn
+    // 2) {set: {key: 'board', value: ...}}
+    // 3) {set: {key: 'delta', value: ...}}]
+    return alphaBetaService.alphaBetaDecision(
+        [null, {set: {key: 'board', value: board}}],
+        playerIndex, getNextStates, getStateScoreForIndex0,
+        // If you want to see debugging output in the console, then surf to game.html?debug
+        window.location.search === '?debug' ? getDebugStateToString : null,
+        alphaBetaLimits);
+  }
+
+  function getStateScoreForIndex0(move) { // alphaBetaService also passes playerIndex, in case you need it: getStateScoreForIndex0(move, playerIndex)
+    if (move[0].endMatch) {
+      var endMatchScores = move[0].endMatch.endMatchScores;
+      return endMatchScores[0] > endMatchScores[1] ? Number.POSITIVE_INFINITY
+          : endMatchScores[0] < endMatchScores[1] ? Number.NEGATIVE_INFINITY
+          : 0;
+    }
+    return 0;
+  }
+
+  function getNextStates(move, playerIndex) {
+    return gameLogic.getPossibleMoves(move[1].set.value, playerIndex);
+  }
+
+  function getDebugStateToString(move) {
+    return "\n" + move[1].set.value.join("\n") + "\n";
+  }
+
+  return {createComputerMove: createComputerMove};
+}]);
