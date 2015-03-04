16e5b94cfb2b5312d87708405e23b336590a1465
yoav-zibin <yoav.zibin@gmail.com>
2015-03-04 11:12:42 -0500

    EMPTY

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,15 +19,43 @@ module.exports = function(grunt) {
           'game.min.html': ['game.html']
         }
       }
-    }
+    },
+    'http-server': {
+        'dev': {
+            // the server root directory
+            root: '.',
+            port: 9000,
+            host: "0.0.0.0",
+            cache: 1,
+            showDir : true,
+            autoIndex: true,
+            // server default file extension
+            ext: "html",
+            // run in parallel with other tasks
+            runInBackground: true
+        }
+    },
+    protractor: {
+      options: {
+        configFile: "protractor.conf.js", // Default config file
+        keepAlive: true, // If false, the grunt process stops when the test fails.
+        noColor: false, // If true, protractor will not use colors in its output.
+        args: {
+          // Arguments passed to the command
+        }
+      },
+      all: {}
+    },
   });
 
-  grunt.loadNpmTasks('grunt-protractor-coverage');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-processhtml');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-http-server');
+  grunt.loadNpmTasks('grunt-protractor-runner');
+  grunt.loadNpmTasks('grunt-protractor-coverage');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify', 'processhtml']);
+  grunt.registerTask('default', ['jshint', 'uglify', 'processhtml', 'http-server', 'protractor']);
 
 };
