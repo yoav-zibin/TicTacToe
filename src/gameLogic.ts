70522c45007611466062c836d6b55a92aa6956c7
bzsmz1990 <bzsmz1990@gmail.com>
2015-09-17 21:59:58 -0400

    begin gamelogic part

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -1,3 +1,40 @@
+/**
+ * This is the logic service for chess. The game board is represented as a
+ * 2D array (9*7). All elements are listed below:
+ *
+ * LionW:      White Lion          LionB:      Black Lion
+ * TigerW:     White Tiger         TigerB:     Black Tiger
+ * DogW:       White Dog           DogB:       Black Dog
+ * CatW:       White Cat           CatB:       Black Cat
+ * MouseW:     White Mouse         MouseB:     Black Mouse
+ * LeopardW:   White Leopard       LeopardB:   Black Leopard
+ * WolfW:      White Wolf          WolfB:      Black Wolf
+ * ElephantW:  White Elephant      ElephantB:  Black Elephant
+ * TrapW:      White Trap          TrapB:      Black Trap
+ * DenW:       White Home          DenB:       Black Home
+ *
+ * L: Land                       R: River
+ *
+ *
+ *
+ * Example - The initial state:
+ *
+ *        0     1     2     3     4     5     6
+ * 0:  [['WLion', 'L', 'WTrap', 'WDen', 'WTrap', 'L', 'WTiger'],
+ * 1:  ['L', 'WDog', 'L', 'WTrap', 'L', 'WCat', 'L'],
+ * 2:  ['WMouse', 'L', 'WLeopard', 'L', 'WWolf', 'L', 'WElephant'],
+ * 3:  ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+ * 4:  ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+ * 5:  ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+ * 6:  ['BElephant', 'L', 'BWolf', 'L', 'BLeopard', 'L', 'BMouse'],
+ * 7:  ['L', 'BCat', 'L', 'BTrap', 'L', 'BDog', 'L'],
+ * 8:  ['BTiger', 'L', 'BTrap', 'BDen', 'BTrap', 'L', 'BLion']]
+ *
+ * Note: The number of row and col are both zero based, so the last row for
+ * white is 0 and for black is 7.
+ *
+ */
+
 type Board = string[][];
 interface BoardDelta {
   row: number;
@@ -9,153 +46,146 @@ interface IState {
 }
 
 module gameLogic {
-
-  /** Returns the initial TicTacToe board, which is a 3x3 matrix containing ''. */
+  /** Returns the initial Jungle board, which is a 9x7 matrix containing ''. */
   export function getInitialBoard(): Board {
-    return [['', '', ''],
-            ['', '', ''],
-            ['', '', '']];
+    return [['WLion', 'L', 'WTrap', 'WDen', 'WTrap', 'L', 'WTiger'],
+			     ['L', 'WDog', 'L', 'WTrap', 'L', 'WCat', 'L'],
+           ['WMouse', 'L', 'WLeopard', 'L', 'WWolf', 'L', 'WElephant'],
+           ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+           ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+           ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+           ['BElephant', 'L', 'BWolf', 'L', 'BLeopard', 'L', 'BMouse'],
+           ['L', 'BCat', 'L', 'BTrap', 'L', 'BDog', 'L'],
+           ['BTiger', 'L', 'BTrap', 'BDen', 'BTrap', 'L', 'BLion']];
   }
 
+  // /**
+  //  * Returns true if the game ended in a tie because there are
+  //  * no available moves for any pieces
+  //  *
+  //  * @board: the board representation
+  //  * @turnIndex: current player (0: Black, 1: White)
+  //  *
+  //  * return true if the game ended in a tie because
+  //  * there are no available moves for any pieces
+  //  */
+  // function isTie(board: Board, turnIndex: number): boolean {
+  //
+  // }
+
+
   /**
-   * Returns true if the game ended in a tie because there are no empty cells.
-   * E.g., isTie returns true for the following board:
-   *     [['X', 'O', 'X'],
-   *      ['X', 'O', 'O'],
-   *      ['O', 'X', 'X']]
+  * Returns turnIndex initial
+  * 0: Black;    1: White
+  */
+  function getTurn(turnIndex) {
+    return (turnIndex === 0 ? 'B' : 'W');
+  }
+
+
+
+  /**
+   * Return the winner (either 'W' or 'B') or '' if there is no winner
+   *
    */
-  function isTie(board: Board): boolean {
-    for (var i = 0; i < 3; i++) {
-      for (var j = 0; j < 3; j++) {
-        if (board[i][j] === '') {
-          // If there is an empty cell then we do not have a tie.
-          return false;
-        }
-      }
-    }
-    // No empty cells, so we have a tie!
-    return true;
+  function getWinner(board: Board, turnIndexBeforeMove: number): string {
+
   }
 
+
   /**
-   * Return the winner (either 'X' or 'O') or '' if there is no winner.
-   * The board is a matrix of size 3x3 containing either 'X', 'O', or ''.
-   * E.g., getWinner returns 'X' for the following board:
-   *     [['X', 'O', ''],
-   *      ['X', 'O', ''],
-   *      ['X', '', '']]
+   * Returns the list of available positions for Elephant to move
    */
-  function getWinner(board: Board): string {
-    var boardString = '';
-    for (var i = 0; i < 3; i++) {
-      for (var j = 0; j < 3; j++) {
-        var cell = board[i][j];
-        boardString += cell === '' ? ' ' : cell;
-      }
-    }
-    var win_patterns = [
-      'XXX......',
-      '...XXX...',
-      '......XXX',
-      'X..X..X..',
-      '.X..X..X.',
-      '..X..X..X',
-      'X...X...X',
-      '..X.X.X..'
-    ];
-    for (i = 0; i < win_patterns.length; i++) {
-      var win_pattern = win_patterns[i];
-      var x_regexp = new RegExp(win_pattern);
-      var o_regexp = new RegExp(win_pattern.replace(/X/g, 'O'));
-      if (x_regexp.test(boardString)) {
-        return 'X';
-      }
-      if (o_regexp.test(boardString)) {
-        return 'O';
-      }
-    }
-    return '';
+  function getElephantPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
   }
 
   /**
-   * Returns all the possible moves for the given board and turnIndexBeforeMove.
+   * Returns the list of available positions for Lion to move
+   */
+  function getLionPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns the list of available positions for Tiger to move
+   */
+  function getTigerPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns the list of available positions for Leopard to move
+   */
+  function getLeopardPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns the list of available positions for Dog to move
+   */
+  function getDogPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns the list of available positions for Wolf to move
+   */
+  function getWolfPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns the list of available positions for Cat to move
+   */
+  function getCatPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns the list of available positions for Rat to move
+   */
+  function getRatPossibleMoves(board: Board,
+    turnIndexBeforeMove: number, deltaFrom: BoardDelta): IMove[] {
+
+  }
+
+  /**
+   * Returns all the possible moves for the given board and turnIndex.
    * Returns an empty array if the game is over.
    */
   export function getPossibleMoves(board: Board, turnIndexBeforeMove: number): IMove[] {
-    var possibleMoves: IMove[] = [];
-    for (var i = 0; i < 3; i++) {
-      for (var j = 0; j < 3; j++) {
-        try {
-          possibleMoves.push(createMove(board, i, j, turnIndexBeforeMove));
-        } catch (e) {
-          // The cell in that position was full.
-        }
-      }
-    }
-    return possibleMoves;
+
   }
+
 
   /**
    * Returns the move that should be performed when player
-   * with index turnIndexBeforeMove makes a move in cell row X col.
+   * with index turnIndex makes a move in cell row X col.
    */
-  export function createMove(
-      board: Board, row: number, col: number, turnIndexBeforeMove: number): IMove {
-    if (!board) {
-      // Initially (at the beginning of the match), the board in state is undefined.
-      board = getInitialBoard();
-    }
-    if (board[row][col] !== '') {
-      throw new Error("One can only make a move in an empty position!");
-    }
-    if (getWinner(board) !== '' || isTie(board)) {
-      throw new Error("Can only make a move if the game is not over!");
-    }
-    var boardAfterMove = angular.copy(board);
-    boardAfterMove[row][col] = turnIndexBeforeMove === 0 ? 'X' : 'O';
-    var winner = getWinner(boardAfterMove);
-    var firstOperation: IOperation;
-    if (winner !== '' || isTie(boardAfterMove)) {
-      // Game over.
-      firstOperation = {endMatch: {endMatchScores:
-        winner === 'X' ? [1, 0] : winner === 'O' ? [0, 1] : [0, 0]}};
-    } else {
-      // Game continues. Now it's the opponent's turn (the turn switches from 0 to 1 and 1 to 0).
-      firstOperation = {setTurn: {turnIndex: 1 - turnIndexBeforeMove}};
-    }
-    var delta: BoardDelta = {row: row, col: col};
-    return [firstOperation,
-            {set: {key: 'board', value: boardAfterMove}},
-            {set: {key: 'delta', value: delta}}];
+  export function createMove( board: Board, deltaFrom: BoardDelta, deltaTo: BoardDelta,
+    turnIndexBeforeMove: number):IMove {
+
   }
 
+
+  /**
+   * Check if the move is OK.
+   *
+   * @returns return true if the move is ok, otherwise false.
+   */
   export function isMoveOk(params: IIsMoveOk): boolean {
-    var move = params.move;
-    var turnIndexBeforeMove = params.turnIndexBeforeMove;
-    var stateBeforeMove: IState = params.stateBeforeMove;
-    // The state and turn after move are not needed in TicTacToe (or in any game where all state is public).
-    //var turnIndexAfterMove = params.turnIndexAfterMove;
-    //var stateAfterMove = params.stateAfterMove;
 
-    // We can assume that turnIndexBeforeMove and stateBeforeMove are legal, and we need
-    // to verify that move is legal.
-    try {
-      // Example move:
-      // [{setTurn: {turnIndex : 1},
-      //  {set: {key: 'board', value: [['X', '', ''], ['', '', ''], ['', '', '']]}},
-      //  {set: {key: 'delta', value: {row: 0, col: 0}}}]
-      var deltaValue: BoardDelta = move[2].set.value;
-      var row = deltaValue.row;
-      var col = deltaValue.col;
-      var board = stateBeforeMove.board;
-      var expectedMove = createMove(board, row, col, turnIndexBeforeMove);
-      if (!angular.equals(move, expectedMove)) {
-        return false;
-      }
-    } catch (e) {
-      // if there are any exceptions then the move is illegal
-      return false;
-    }
-    return true;
   }
+
+
+
+
 }
