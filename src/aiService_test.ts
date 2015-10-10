8f3c2ad9ef2281a02537e3a85bd38da17b2d3ca5
bzsmz1990 <bzsmz1990@gmail.com>
2015-10-10 00:51:05 -0400

    add one more test case

--- src/aiService_test.ts
+++ src/aiService_test.ts
@@ -31,4 +31,35 @@ describe("aiService", function() {
     expect(angular.equals(move, expectMove)).toBe(true);
   });
 
+  it("White finds an immediate winning move by moving WMouse to 8*3", function() {
+    let board: Board = [['L', 'L', 'WTrap', 'WDen', 'WTrap', 'L', 'L'],
+                        ['L', 'L', 'L', 'WTrap', 'L', 'L', 'L'],
+                        ['L', 'L', 'L', 'L', 'WWolf', 'L', 'WElephant'],
+                        ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+                        ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+                        ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+                        ['L', 'L', 'L', 'L', 'L', 'L', 'L'],
+                        ['L', 'BCat', 'L', 'BTrap', 'L', '', 'L'],
+                        ['L', 'L', 'WMouse', 'BDen', 'BTrap', 'L', 'L']];
+
+    let move: IMove = aiService.createComputerMove(board, 1, {maxDepth: 1});
+
+    let expectMove: IMove =
+        [{endMatch: {endMatchScores: [0, 1]}},
+          {set: {key: 'board', value:
+            [['L', 'L', 'WTrap', 'WDen', 'WTrap', 'L', 'L'],
+            ['L', 'L', 'L', 'WTrap', 'L', 'L', 'L'],
+            ['L', 'L', 'L', 'L', 'WWolf', 'L', 'WElephant'],
+            ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+            ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+            ['L', 'R', 'R', 'L', 'R', 'R', 'L'],
+            ['L', 'L', 'L', 'L', 'L', 'L', 'L'],
+            ['L', 'BCat', 'L', 'BTrap', 'L', '', 'L'],
+            ['L', 'L', 'BTrap', 'WMouse', 'BTrap', 'L', 'L']]}},
+        {set: {key: 'deltaFrom', value: {row: 8, col: 2}}},
+        {set: {key: 'deltaTo', value: {row: 8, col: 3}}}];
+
+    expect(angular.equals(move, expectMove)).toBe(true);
+  });
+
 });
