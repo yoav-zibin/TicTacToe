16f6cbd5ca7cff3b27005798220630457f713f23
bzsmz1990 <bzsmz1990@gmail.com>
2015-10-28 21:48:27 -0400

    modify language

--- languages/en.js
+++ languages/en.js
@@ -1,6 +1,10 @@
 window.angularTranslationsLoaded('en',{
-"RULES_OF_TICTACTOE":"Rules of TicTacToe",
-"RULES_SLIDE1":"You and your opponent take turns to mark the grid in an empty spot. The first mark is X, then O, then X, then O, etc.",
-"RULES_SLIDE2":"The first to mark a whole row, column or diagonal wins.",
+"RULES_OF_JUNGLE": "Rules of Jungle",
+"RULES_SLIDE1": "There are two players, black and white. Black goes first. Each player has eight different pieces representing different animals.",
+"RULES_SLIDE2": "Higher ranking pieces can capture all pieces of identical or weaker ranking. However there is one exception: The mouse may capture the elephant, while the elephant cannot capture the mouse",
+"RULES_SLIDE3": "The animal ranking, from strongest to weakest, is: Elephant, Lion, Tiger, Leopard, Wolf, Dog, Cat, Mouse",
+"RULES_SLIDE4": "The rat is the only animal that is allowed to go onto a water square. The rat may not capture the elephant or another rat on land directly from a water square.",
+"RULES_SLIDE5": "Lions and tigers are able to leap over water (either horizontally or vertically). They cannot jump over the water when a rat is on any of the intervening water squares.",
+"RULES_SLIDE6": "When a piece is in an opponent's trap, any of the opponent's pieces may capture it regardless its strength. A piece in one of its own traps is unaffected.",
 "CLOSE":"Close"
 });
--- languages/he.js
+++
@@ -1,6 +0,0 @@
-window.angularTranslationsLoaded('he',{
-"RULES_OF_TICTACTOE":"חוקי איקס עיגול",
-"RULES_SLIDE1":"אתה והיריב מסמנים איקס ועיגול בתורות במקומות הפנויים בלוח",
-"RULES_SLIDE2":"הראשון שמסמן שורה, עמודה או אלכסון מנצח",
-"CLOSE":"סגור"
-});
