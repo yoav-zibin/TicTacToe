5d8d7f06c301826dfc144909bb4694dc0108e2ed
yoav_zibin <yoav.zibin@gmail.com>
2016-01-05 09:55:14 -0500

    x

--- src/game.ts
+++ src/game.ts
@@ -130,7 +130,7 @@ module game {
 angular.module('myApp', ['ngTouch', 'ui.bootstrap', 'gameServices'])
   .run(function () {
     $rootScope['game'] = game;
-    translate.setLanguage('en',  {
+    translate.setLanguage('en', {
       RULES_OF_TICTACTOE: "Rules of TicTacToe",
       RULES_SLIDE1: "You and your opponent take turns to mark the grid in an empty spot. The first mark is X, then O, then X, then O, etc.",
       RULES_SLIDE2: "The first to mark a whole row, column or diagonal wins.",
