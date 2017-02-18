0c97bc8f217efcd0bb626c977cfb349954044049
yomancool <tp65l4m06cl3@gmail.com>
2017-02-18 14:17:30 -0500

    install

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-02-02T22:03:00.304Z';
+var CACHE_NAME = 'cache-v2017-02-18T19:15:47.978Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
