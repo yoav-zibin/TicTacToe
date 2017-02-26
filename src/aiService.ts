8658cf59ee2474176fdbd7d335fff95ddee887b8
Mengdi <wangmengdi@gmail.com>
2017-02-26 16:01:30 -0500

    fix bug in ai.ts

--- src/aiService.ts
+++ src/aiService.ts
@@ -2,8 +2,8 @@ module aiService {
   /** Returns the move that the computer player should do for the given state in move. */
   export function findComputerMove(move: IMove): IMove {
     return createComputerMove(move,
-        // at most 1 second for the AI to choose a move (but might be much quicker)
-        {millisecondsLimit: 1000});
+      // at most 1 second for the AI to choose a move (but might be much quicker)
+      { millisecondsLimit: 1000 });
   }
 
   /**
@@ -14,11 +14,14 @@ module aiService {
     let possibleMoves: IMove[] = [];
     for (let i = 0; i < gameLogic.ROWS; i++) {
       for (let j = 0; j < gameLogic.COLS; j++) {
-        try {
-          possibleMoves.push(gameLogic.createMove(state, i, j, turnIndexBeforeMove));
-        } catch (e) {
-          // The cell in that position was full.
+        for (let shapeId = 0; shapeId < gameLogic.OPERATIONS; shapeId++) {
+          try {
+            possibleMoves.push(gameLogic.createMove(state, i, j, shapeId, turnIndexBeforeMove));
+          } catch (e) {
+            // The cell in that position was full.
+          }
         }
+
       }
     }
     return possibleMoves;
@@ -31,17 +34,17 @@ module aiService {
    * millisecondsLimit is a time limit, and maxDepth is a depth limit.
    */
   export function createComputerMove(
-      move: IMove, alphaBetaLimits: IAlphaBetaLimits): IMove {
+    move: IMove, alphaBetaLimits: IAlphaBetaLimits): IMove {
     // We use alpha-beta search, where the search states are TicTacToe moves.
     return alphaBetaService.alphaBetaDecision(
-        move, move.turnIndex, getNextStates, getStateScoreForIndex0, null, alphaBetaLimits);
+      move, move.turnIndex, getNextStates, getStateScoreForIndex0, null, alphaBetaLimits);
   }
 
   function getStateScoreForIndex0(move: IMove, playerIndex: number): number {
     let endMatchScores = move.endMatchScores;
     if (endMatchScores) {
       return endMatchScores[0] > endMatchScores[1] ? Number.POSITIVE_INFINITY
-          : endMatchScores[0] < endMatchScores[1] ? Number.NEGATIVE_INFINITY
+        : endMatchScores[0] < endMatchScores[1] ? Number.NEGATIVE_INFINITY
           : 0;
     }
     return 0;
