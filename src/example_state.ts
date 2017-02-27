2c66b3034d10e46a48f3fbbff0d65e1f42cc31ec
libertylocked <unlock.liberty@gmail.com>
2017-02-27 15:38:32 -0500

    fix pocket positions

--- src/example_state.ts
+++ src/example_state.ts
@@ -17,8 +17,8 @@ var render = Render.create({
     element: document.getElementById("canvas-container"),
     engine: engine,
     options: {
-        height: 600,
-        width: 420,
+        height: 550,
+        width: 410,
     }
 });
 
@@ -44,32 +44,47 @@ World.add(world, [
 // create pockets
 World.add(world, [
     // top-left
-    Bodies.circle(30, 20, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    Bodies.circle(20, 15, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
     // top-right
-    Bodies.circle(380, 20, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    Bodies.circle(390, 15, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
     // left
     Bodies.circle(5, 275, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
     // right
     Bodies.circle(405, 275, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
     // bottom-left
-    Bodies.circle(30, 530, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    Bodies.circle(20, 535, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
     // bottom-right
-    Bodies.circle(380, 530, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    Bodies.circle(390, 535, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
 ]);
 
-var collisionCategoryCue = 0x0001;
-var collisionCategoryNormalBalls = 0x0002;
-var collisionMaskAllBalls = 0x0003;
-var collisionMaskCueOnly = 0x0001;
+// bunch of game play constants
+module GameplayConsts {
+    export const CollisionCategoryCue = 0x0001;
+    export const CollisionCategoryNormalBalls = 0x0002;
+    export const CollisionMaskAllBalls = 0x0003;
+    export const CollisionMaskCueOnly = 0x0001;
+
+    export const BallRestitution = 0.9;
+    export const BallFriction = 0.3;
+};
 
 // create ball bodies
 World.add(world, [
     // cue
-    Bodies.circle(100, 350, 10, <any>{isStatic: false, collisionFilter: { category: collisionCategoryCue, mask: collisionMaskAllBalls }, restitution: 0.9, friction: 0.2, render: {fillStyle:'white', strokeStyle:'black'}}),
+    Bodies.circle(100, 350, 10, <any>{isStatic: false, 
+        collisionFilter: { category: GameplayConsts.CollisionCategoryCue, mask: GameplayConsts.CollisionMaskAllBalls }, 
+        restitution: GameplayConsts.BallRestitution, friction: GameplayConsts.BallFriction, 
+        render: {fillStyle:'white', strokeStyle:'black'}}),
     // eight ball
-    Bodies.circle(130, 160, 10, <any>{isStatic: false, collisionFilter: { category: collisionCategoryNormalBalls, mask: collisionMaskAllBalls }, restitution: 0.9, friction: 0.2, render: {fillStyle:'black', strokeStyle:'black'}}),
+    Bodies.circle(130, 160, 10, <any>{isStatic: false, 
+        collisionFilter: { category: GameplayConsts.CollisionCategoryNormalBalls, mask: GameplayConsts.CollisionMaskAllBalls }, 
+        restitution: GameplayConsts.BallRestitution, friction: GameplayConsts.BallFriction, 
+        render: {fillStyle:'black', strokeStyle:'black'}}),
     // a solid ball
-    Bodies.circle(350, 500, 10, <any>{isStatic: false, collisionFilter: { category: collisionCategoryNormalBalls, mask: collisionMaskAllBalls }, restitution: 0.9, friction: 0.2, render: {fillStyle:'pink', strokeStyle:'black'}}),
+    Bodies.circle(350, 500, 10, <any>{isStatic: false, 
+        collisionFilter: { category: GameplayConsts.CollisionCategoryNormalBalls, mask: GameplayConsts.CollisionMaskAllBalls }, 
+        restitution: GameplayConsts.BallRestitution, friction: GameplayConsts.BallFriction, 
+        render: {fillStyle:'pink', strokeStyle:'black'}}),
 ]);
 
 let mouse = Mouse.create(render.canvas),
@@ -82,7 +97,7 @@ let mouse = Mouse.create(render.canvas),
             }
         }
     });
-mouseConstraint.collisionFilter.mask = collisionMaskCueOnly;
+mouseConstraint.collisionFilter.mask = GameplayConsts.CollisionMaskCueOnly;
 
 World.add(world, mouseConstraint);
 
--- ts_output_readonly_do_NOT_change_manually/src/example_state.js
+++ ts_output_readonly_do_NOT_change_manually/src/example_state.js
@@ -4,8 +4,8 @@ var render = Render.create({
     element: document.getElementById("canvas-container"),
     engine: engine,
     options: {
-        height: 600,
-        width: 420,
+        height: 550,
+        width: 410,
     }
 });
 engine.world.gravity.y = 0;
@@ -28,30 +28,46 @@ World.add(world, [
 // create pockets
 World.add(world, [
     // top-left
-    Bodies.circle(30, 20, 25, { isStatic: true, render: { fillStyle: 'black' } }),
+    Bodies.circle(20, 15, 31, { isStatic: true, render: { fillStyle: 'black' } }),
     // top-right
-    Bodies.circle(380, 20, 25, { isStatic: true, render: { fillStyle: 'black' } }),
+    Bodies.circle(390, 15, 31, { isStatic: true, render: { fillStyle: 'black' } }),
     // left
     Bodies.circle(5, 275, 31, { isStatic: true, render: { fillStyle: 'black' } }),
     // right
     Bodies.circle(405, 275, 31, { isStatic: true, render: { fillStyle: 'black' } }),
     // bottom-left
-    Bodies.circle(30, 530, 25, { isStatic: true, render: { fillStyle: 'black' } }),
+    Bodies.circle(20, 535, 31, { isStatic: true, render: { fillStyle: 'black' } }),
     // bottom-right
-    Bodies.circle(380, 530, 25, { isStatic: true, render: { fillStyle: 'black' } }),
+    Bodies.circle(390, 535, 31, { isStatic: true, render: { fillStyle: 'black' } }),
 ]);
-var collisionCategoryCue = 0x0001;
-var collisionCategoryNormalBalls = 0x0002;
-var collisionMaskAllBalls = 0x0003;
-var collisionMaskCueOnly = 0x0001;
+// bunch of game play constants
+var GameplayConsts;
+(function (GameplayConsts) {
+    GameplayConsts.CollisionCategoryCue = 0x0001;
+    GameplayConsts.CollisionCategoryNormalBalls = 0x0002;
+    GameplayConsts.CollisionMaskAllBalls = 0x0003;
+    GameplayConsts.CollisionMaskCueOnly = 0x0001;
+    GameplayConsts.BallRestitution = 0.9;
+    GameplayConsts.BallFriction = 0.3;
+})(GameplayConsts || (GameplayConsts = {}));
+;
 // create ball bodies
 World.add(world, [
     // cue
-    Bodies.circle(100, 350, 10, { isStatic: false, collisionFilter: { category: collisionCategoryCue, mask: collisionMaskAllBalls }, restitution: 0.9, friction: 0.2, render: { fillStyle: 'white', strokeStyle: 'black' } }),
+    Bodies.circle(100, 350, 10, { isStatic: false,
+        collisionFilter: { category: GameplayConsts.CollisionCategoryCue, mask: GameplayConsts.CollisionMaskAllBalls },
+        restitution: GameplayConsts.BallRestitution, friction: GameplayConsts.BallFriction,
+        render: { fillStyle: 'white', strokeStyle: 'black' } }),
     // eight ball
-    Bodies.circle(130, 160, 10, { isStatic: false, collisionFilter: { category: collisionCategoryNormalBalls, mask: collisionMaskAllBalls }, restitution: 0.9, friction: 0.2, render: { fillStyle: 'black', strokeStyle: 'black' } }),
+    Bodies.circle(130, 160, 10, { isStatic: false,
+        collisionFilter: { category: GameplayConsts.CollisionCategoryNormalBalls, mask: GameplayConsts.CollisionMaskAllBalls },
+        restitution: GameplayConsts.BallRestitution, friction: GameplayConsts.BallFriction,
+        render: { fillStyle: 'black', strokeStyle: 'black' } }),
     // a solid ball
-    Bodies.circle(350, 500, 10, { isStatic: false, collisionFilter: { category: collisionCategoryNormalBalls, mask: collisionMaskAllBalls }, restitution: 0.9, friction: 0.2, render: { fillStyle: 'pink', strokeStyle: 'black' } }),
+    Bodies.circle(350, 500, 10, { isStatic: false,
+        collisionFilter: { category: GameplayConsts.CollisionCategoryNormalBalls, mask: GameplayConsts.CollisionMaskAllBalls },
+        restitution: GameplayConsts.BallRestitution, friction: GameplayConsts.BallFriction,
+        render: { fillStyle: 'pink', strokeStyle: 'black' } }),
 ]);
 var mouse = Mouse.create(render.canvas), mouseConstraint = MouseConstraint.create(engine, {
     mouse: mouse,
@@ -62,7 +78,7 @@ var mouse = Mouse.create(render.canvas), mouseConstraint = MouseConstraint.creat
         }
     }
 });
-mouseConstraint.collisionFilter.mask = collisionMaskCueOnly;
+mouseConstraint.collisionFilter.mask = GameplayConsts.CollisionMaskCueOnly;
 World.add(world, mouseConstraint);
 // keep the mouse in sync with rendering
 render.mouse = mouse;
