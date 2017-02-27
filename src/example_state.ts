8219a8be2ad658f0432cc921cac1eb6aaf3d9dac
libertylocked <unlock.liberty@gmail.com>
2017-02-27 13:33:05 -0500

    table with pockets and 3 balls

--- src/example_state.ts
+++ src/example_state.ts
@@ -23,16 +23,48 @@ var render = Render.create({
 });
 
 engine.world.gravity.y = 0;
+let renderOptions = <any>render.options;
+renderOptions.wireframes = false;
+renderOptions.background = 'green';
 
-
-// create terrain
+// create terrain (table frames)
 World.add(world, [
-    Bodies.rectangle(30, 150, 5, 200, {isStatic: true, render: <any>{fillStyle:'black', strokeStyle:'black'}}),
-    Bodies.rectangle(30, 400, 5, 200, {isStatic: true, render: <any>{fillStyle:'black', strokeStyle:'black'}}),
-    Bodies.rectangle(200, 20, 300, 5, {isStatic: true, render: <any>{fillStyle:'black', strokeStyle:'black'}}),
-    Bodies.rectangle(200, 530, 300, 5, {isStatic: true, render: <any>{fillStyle:'black', strokeStyle:'black'}}),
-    Bodies.rectangle(380, 150, 5, 200, {isStatic: true, render: <any>{fillStyle:'black', strokeStyle:'black'}}),
-    Bodies.rectangle(380, 400, 5, 200, {isStatic: true, render: <any>{fillStyle:'black', strokeStyle:'black'}}),
+    // left
+    Bodies.rectangle(30, 150, 5, 210, {isStatic: true, render: <any>{fillStyle:'#825201', strokeStyle:'black'}}),
+    Bodies.rectangle(30, 400, 5, 210, {isStatic: true, render: <any>{fillStyle:'#825201', strokeStyle:'black'}}),
+    // top
+    Bodies.rectangle(205, 20, 305, 5, {isStatic: true, render: <any>{fillStyle:'#825201', strokeStyle:'black'}}),
+    // bottom
+    Bodies.rectangle(205, 530, 305, 5, {isStatic: true, render: <any>{fillStyle:'#825201', strokeStyle:'black'}}),
+    // right
+    Bodies.rectangle(380, 150, 5, 210, {isStatic: true, render: <any>{fillStyle:'#825201', strokeStyle:'black'}}),
+    Bodies.rectangle(380, 400, 5, 210, {isStatic: true, render: <any>{fillStyle:'#825201', strokeStyle:'black'}}),
+]);
+
+// create pockets
+World.add(world, [
+    // top-left
+    Bodies.circle(30, 20, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    // top-right
+    Bodies.circle(380, 20, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    // left
+    Bodies.circle(5, 275, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    // right
+    Bodies.circle(405, 275, 31, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    // bottom-left
+    Bodies.circle(30, 530, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+    // bottom-right
+    Bodies.circle(380, 530, 25, {isStatic: true, render: <any>{fillStyle:'black'}}),
+]);
+
+// create ball bodies
+World.add(world, [
+    // cue
+    Bodies.circle(100, 350, 10, <any>{isStatic: false, render: {fillStyle:'white', strokeStyle:'black'}}),
+    // eight ball
+    Bodies.circle(130, 160, 10, <any>{isStatic: false, render: {fillStyle:'black', strokeStyle:'black'}}),
+    // a solid ball
+    Bodies.circle(350, 500, 10, <any>{isStatic: false, render: {fillStyle:'pink', strokeStyle:'black'}}),
 ]);
 
 Render.run(render);
