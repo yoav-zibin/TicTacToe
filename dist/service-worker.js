d872b3cb4757bb9506cdf1c3745e4d978a430535
Yingying <yingyings.ice@gmail.com>
2017-04-23 16:57:56 -0400

    change disabled button style, grunt

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-23T20:51:06.649Z';
+var CACHE_NAME = 'cache-v2017-04-23T20:57:23.369Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
