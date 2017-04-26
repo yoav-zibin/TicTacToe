f3b3a16670cd4c4683590e379fa990ab67493e2d
Yingying <yingyings.ice@gmail.com>
2017-04-26 13:23:20 -0400

    grunt

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-04-26T14:37:19.732Z';
+var CACHE_NAME = 'cache-v2017-04-26T17:21:51.687Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
