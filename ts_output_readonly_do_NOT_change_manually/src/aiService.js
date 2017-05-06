9d2d939a32bb15f1cf2d27c3363d944f946b9d8c
Yingying <yingyings.ice@gmail.com>
2017-05-06 11:28:23 -0400

    md button

--- ts_output_readonly_do_NOT_change_manually/src/aiService.js
+++ ts_output_readonly_do_NOT_change_manually/src/aiService.js
@@ -4,7 +4,7 @@ var aiService;
     function findComputerMove(move) {
         return createComputerMove(move, 
         // at most 1 second for the AI to choose a move (but might be much quicker)
-        { millisecondsLimit: 1000, maxDepth: 2000 });
+        { millisecondsLimit: 1000, maxDepth: 3000 });
     }
     aiService.findComputerMove = findComputerMove;
     /**
